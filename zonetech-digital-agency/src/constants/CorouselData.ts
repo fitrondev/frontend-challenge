0a9ccd6ecec7833fd8994f90167513017227eba8
fitronansori <fitronansori7@gmail.com>
2024-03-06 23:07:51 +0800

    Add CorouselData

---
+++ zonetech-digital-agency/src/constants/CorouselData.ts
@@ -0,0 +1,28 @@
+// images
+import Profile1 from "../assets/images/p1.jpg";
+import Profile2 from "../assets/images/p2.jpg";
+import Profile3 from "../assets/images/p3.jpg";
+
+export const CorouselData = [
+  {
+    id: 1,
+    name: "Fitron Ansori",
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in justo sollicitudin, lobortis turpis vel, dapibus tellus. Lorem ipsum dolor.",
+    image: Profile1,
+  },
+  {
+    id: 2,
+    name: "Azkia Sururi",
+    comment:
+      "Donec a feugiat sapien. Quisque vulputate efficitur lorem, vel molestie ligula mattis vitae. Nullam egestas lorem et pulvinar dapibus.",
+    image: Profile2,
+  },
+  {
+    id: 3,
+    name: "Nara Husnina",
+    comment:
+      "Ut a maximus neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent imperdiet diam.",
+    image: Profile3,
+  },
+];
