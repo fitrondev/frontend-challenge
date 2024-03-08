b556125d9fc3637be1257c06b1a90b23565cdf8b
fitronansori <fitronansori7@gmail.com>
2024-03-08 18:58:48 +0800

    Add TeamsData constant with team information

---
+++ zonetech-digital-agency/src/constants/TeamsData.ts
@@ -0,0 +1,57 @@
+import Team1 from "../assets/images/Team1.jpg";
+import Team2 from "../assets/images/Team2.jpg";
+import Team3 from "../assets/images/Team3.jpg";
+import Team4 from "../assets/images/Team4.jpg";
+import Team5 from "../assets/images/Team5.jpg";
+import Team6 from "../assets/images/Team6.jpg";
+
+export const TeamsData = [
+  {
+    id: 1,
+    name: "Fitron Ansori",
+    position: "CEO",
+    images: Team1,
+    comment:
+      "Fusce iaculis tincidunt fermentum. Nam tempor viverra leo a fringilla. Etiam eget nulla nec dui facilisis dignissim. Duis vel cursus.",
+  },
+  {
+    id: 2,
+    name: "Felix Kjellberg",
+    position: "CTO",
+    images: Team2,
+    comment:
+      "Quisque eu consequat erat. Nam diam massa, varius at auctor vel, consequat eu purus. Sed feugiat arcu sed lectus dignissim.",
+  },
+  {
+    id: 3,
+    name: "Ardan Dimyathi",
+    position: "Marketing Manager",
+    images: Team3,
+    comment:
+      "Nullam ultricies vehicula urna et ultricies. Maecenas fringilla molestie lacus at molestie. Suspendisse pulvinar viverra dui sit amet semper. Cras.",
+  },
+  {
+    id: 4,
+    name: "Astriani",
+    position: "Engineering Manager",
+    images: Team4,
+    comment:
+      "Nullam ultricies vehicula urna et ultricies. Maecenas fringilla molestie lacus at molestie. Suspendisse pulvinar viverra dui sit amet semper. Cras.",
+  },
+  {
+    id: 5,
+    name: "Skyler White",
+    position: "Tech Lead",
+    images: Team5,
+    comment:
+      "Nullam ultricies vehicula urna et ultricies. Maecenas fringilla molestie lacus at molestie. Suspendisse pulvinar viverra dui sit amet semper. Cras.",
+  },
+  {
+    id: 6,
+    name: "Jesse Pinkman",
+    position: "Developer",
+    images: Team6,
+    comment:
+      "Nullam ultricies vehicula urna et ultricies. Maecenas fringilla molestie lacus at molestie. Suspendisse pulvinar viverra dui sit amet semper. Cras.",
+  },
+];
