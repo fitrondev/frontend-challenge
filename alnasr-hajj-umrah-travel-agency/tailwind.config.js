d8f0a3c6524774175fcdbe17d89ec7d8102da1a9
fitronansori <fitronansori7@gmail.com>
2024-03-14 06:40:57 +0700

    Update Tailwind CSS configuration

--- alnasr-hajj-umrah-travel-agency/tailwind.config.js
+++ alnasr-hajj-umrah-travel-agency/tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{html,js,ts,jsx,tsx}",
+  ],
   theme: {
     container: {
       center: true,
       padding: {
-        DEFAULT: "1rem",
-        md: "2rem",
-        lg: "4rem",
-        xl: "5rem",
+        DEFAULT: "1.5rem",
         "2xl": "100px",
       },
       screens: {
-        sm: "480px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
         "2xl": "1440px",
       },
     },
@@ -23,14 +20,15 @@ export default {
       nunito: ["Nunito", "sans-serif"],
       kufam: ["Kufam", "sans-serif"],
     },
-    colors: {
-      orange: "#FFC265",
-      darkGreen: "#1DA599",
-      lightGreen: "#C1FFFA",
-      textHeading: "#4A4A4A",
-      textPara: "#454545",
+    extend: {
+      colors: {
+        darkOrange: "#FFC265",
+        darkGreen: "#1DA599",
+        lightGreen: "#C1FFFA",
+        textHeading: "#4A4A4A",
+        textPara: "#454545",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
