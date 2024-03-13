b935e16d133dcd5c14c5bf20ed238a15dab3298c
fitronansori <fitronansori7@gmail.com>
2024-03-14 06:41:10 +0700

    Add navigation links to data.ts

---
+++ alnasr-hajj-umrah-travel-agency/src/constants/data.ts
@@ -0,0 +1,26 @@
+export const navLinks = [
+  {
+    id: "#home",
+    title: "Home",
+  },
+
+  {
+    id: "#about",
+    title: "About Us",
+  },
+
+  {
+    id: "#packages",
+    title: "Packages",
+  },
+
+  {
+    id: "#facilities",
+    title: "Facilities",
+  },
+
+  {
+    id: "#gallery",
+    title: "Gallery",
+  },
+];
