df0e767b93d288af1d019749542d24ae7c453b91
fitronansori <fitronansori7@gmail.com>
2024-03-16 08:16:51 +0700

    Add Card component

---
+++ alnasr-hajj-umrah-travel-agency/src/components/ui/Card.tsx
@@ -0,0 +1,9 @@
+type CardProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const Card: React.FC<CardProps> = ({ children, className }) => {
+  return <div className={`card ${className}`}>{children}</div>;
+};
+export default Card;
